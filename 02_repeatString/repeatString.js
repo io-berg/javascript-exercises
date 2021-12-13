b24a91c1baaeb7fca061e55a83f5ee9d357a8dcd
Daniel Berg <Berg.ioDaniel@gmail.com>
2021-12-13 20:25:16 +0100

    first 3

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
+const repeatString = function(string, int) {
 
+    if (int < 0) return 'ERROR';
+
+    let out = '';
+    for (let i = 0; i < int; i++){
+        out += string;
+    }
+    return out;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
 const reverseString = function(str) {
+    let reveresed = '';
+    
+    for (let i = str.length - 1; i >= 0; i--)
+    {
+        reveresed += str[i];
+    }
 
+    return reveresed;
 };
 
 // Do not edit below this line
