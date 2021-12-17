1200c2a4f2ca256af77fd84d6add0dd0ba360e09
Daniel Berg <Berg.ioDaniel@gmail.com>
2021-12-17 17:57:42 +0100

    c

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,12 @@
-const ftoc = function() {
+const ftoc = function(input) {
+  let c = (input - 32) * 5 / 9;
 
+  return Math.round(c * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(input) {
+  let f =  input * 1.8000 + 32;
+  return Math.round(f * 10) / 10;
 };
 
 // Do not edit below this line
