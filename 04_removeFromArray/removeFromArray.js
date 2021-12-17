31fb95b409dcd99961d2e670cd0260b0b6c91ea3
Daniel Berg <Berg.ioDaniel@gmail.com>
2021-12-17 17:59:03 +0100

    4 - 7 complete

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
 
+    for (let i = 0; i < arr.length; i++) {
+        if (args.includes(arr[i]))
+        {
+            arr.splice(i, 1);
+            i -= 1;
+        }
+    }
+    
+    return arr;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (a < 0 || b < 0) return 'ERROR';
+    if (typeof a != "number" || typeof b != "number") return 'ERROR';
 
+    let small = a
+    let big = b;
+
+    if (b < a){
+        small = b;
+        big = a;
+    }
+
+    let sum = 0;
+    for (let i = small; i <= big; i++){
+        sum += i;
+    };
+
+    return sum;
 };
 
 // Do not edit below this line
